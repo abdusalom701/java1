eecae6fb31e0262f8cb11c220282af994dec6593
abdusalom701 <ulugbekovabdusalom@gmail.com>
2024-08-14 17:26:58 +0500

    efjhng

---
+++ script.js
@@ -0,0 +1,13 @@
+var a = prompt ("ismingizni yozing")
+var b = prompt ("yoshingizni yozing")
+var c = prompt ("familiya yozing")
+alert ("xush kelibsiz")
+
+console.log(a+b+c);
+ 
+console.log("ismingiz")
+console.log("yoshingiz")
+console.log("familiyangiz")
+
+
+
